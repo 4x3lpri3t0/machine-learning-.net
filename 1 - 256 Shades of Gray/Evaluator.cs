fab950a4aeb424f4f7bbbba7efe670a7d6578552
Axel Prieto <prietoaxel@gmail.com>
2016-08-23 21:28:44 -0300

    Daily programming practice

---
+++ 1 - 256 Shades of Gray/Evaluator.cs
@@ -0,0 +1,28 @@
+﻿using _1___256_Shades_of_Gray.Classifiers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1___256_Shades_of_Gray
+{
+    public class Evaluator
+    {
+        public static double Correct(
+            IEnumerable<Observation> validationSet,
+            IClassifier classifier)
+        {
+            return validationSet
+                .Select(obs => Score(obs, classifier))
+                .Average();
+        }
+
+        private static double Score(
+            Observation obs,
+            IClassifier classifier)
+        {
+            if (classifier.Predict(obs.Pixels) == obs.Label)
+                return 1.0;
+            else
+                return 0.0;
+        }
+    }
+}
--- 1 - 256 Shades of Gray/Program.cs
+++ 1 - 256 Shades of Gray/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _1___256_Shades_of_Gray.Classifiers;
+using _1___256_Shades_of_Gray.Distances;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +12,18 @@ namespace _1___256_Shades_of_Gray
     {
         static void Main()
         {
+            var distance = new ManhattanDistance();
+            var classifier = new BasicClassifier(distance);
+
             var trainingPath = @"..\..\trainingsample.csv";
             var training = DataReader.ReadObservations(trainingPath);
+            classifier.Train(training);
+
+            var validationPath = @"..\..\validationsample.csv";
+            var validation = DataReader.ReadObservations(validationPath);
+
+            var correct = Evaluator.Correct(validation, classifier);
+            Console.WriteLine("Correctly classified: {0:P2}", correct);
 
             Console.ReadLine();
         }
