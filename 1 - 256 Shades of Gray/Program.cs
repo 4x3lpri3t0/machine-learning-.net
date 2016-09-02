c818c3202865dc1f7b982f720a72fd4eebdc9a80
Axel Prieto <prietoaxel@gmail.com>
2016-09-01 21:45:33 -0300

    Daily programming practice

--- 1 - 256 Shades of Gray/Program.cs
+++ 1 - 256 Shades of Gray/Program.cs
@@ -1,10 +1,6 @@
 ﻿using _1___256_Shades_of_Gray.Classifiers;
 using _1___256_Shades_of_Gray.Distances;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _1___256_Shades_of_Gray
 {
--- FSharp/Script.fsx
+++ FSharp/Script.fsx
@@ -1,8 +1,10 @@
 ﻿open System.IO
 
-type Observation = { Label : string; Pixels : int [] }
+type Observation = { Label:string; Pixels: int[] }
+type Distance = int[] * int[] -> int
+type Classifier = int[] -> string
 
-let toObservation (csvData : string) = 
+let toObservation (csvData:string) =
     let columns = csvData.Split(',')
     let label = columns.[0]
     let pixels = columns.[1..] |> Array.map int
@@ -20,3 +22,39 @@ let manhattanDistance (pixels1,pixels2) =
     Array.zip pixels1 pixels2
     |> Array.map (fun (x,y) -> abs (x-y))
     |> Array.sum
+
+let euclideanDistance (pixels1,pixels2) =
+    Array.zip pixels1 pixels2
+    |> Array.map (fun (x,y) -> pown (x-y) 2)
+    |> Array.sum
+
+let train (trainingset:Observation[]) (dist:Distance) =
+    let classify (pixels:int[]) =
+        trainingset
+        |> Array.minBy (fun x -> dist (x.Pixels, pixels))
+        |> fun x -> x.Label
+    classify
+
+let validationPath = @"C:\Users\Axel\Desktop\machine-learning-.net\1 - 256 Shades of Gray\validationsample.csv"
+let validationData = reader validationPath
+
+let evaluate validationSet classifier = 
+    validationSet
+    |> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+    |> printfn "Correct: %.3f"
+
+let manhattanModel = train trainingData manhattanDistance
+let euclideanModel = train trainingData euclideanDistance
+
+printfn "Manhattan"
+evaluate validationData manhattanModel
+printfn "Euclidean"
+evaluate validationData euclideanModel
+
+// Illustration: full distance function
+
+let d (X,Y) = 
+    Array.zip X Y 
+    |> Array.map (fun (x,y) -> pown (x-y) 2) 
+    |> Array.sum 
+    |> sqrt
